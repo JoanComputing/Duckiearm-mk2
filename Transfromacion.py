b24153dae13bf795ae9f16052b73e7148a302830
Sebastián Herrera <90007170+Kitsuneden02@users.noreply.github.com>
2021-12-01 13:34:30 -0300

    Transformacion

---
+++ Transfromacion.py
@@ -0,0 +1,24 @@
+import math as m
+import numpy as np
+
+O1 = False
+O2 = False
+T = np.array([[0,0,0,2],[0,0,0,3],[0,0,0,4],[0,0,0,0]])
+print("Mientras tanto, la Traslación será: \n",T)
+contador = 0
+Rx = np.array([[1,0,0,0],[0,-1,-(0),0],[0,0,-1,0],[0,0,0,1]])
+Rz = np.array([[0,-(1),0,0],[1,0,0,0],[0,0,1,0],[0,0,0,1]])
+R = np.dot(Rx,Rz)
+Tf = T+R
+print("Transformación final:\n",Tf)
+while 1:
+
+    print("Inserte un punto a transformar. (Cámara)")
+    x = int(input("Coordenada X: ->"))
+    y = int(input("Coordenada Y: ->"))
+    z = int(input("Coordenada Z: ->"))
+    punto = np.array([[x],[y],[z],[1]])
+    puntoTransformado = np.dot(Tf,punto)
+
+    print("Su punto inicial es: \n",punto,"\nY su punto transformado es: \n", puntoTransformado)
+    print("---------------------------------------------------------------")
